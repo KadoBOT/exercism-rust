5f48c8f38d5b11010fee9d200821d68d28d734f4
KadoBOT <ricardo.ambrogi@gmail.com>
2020-06-28 10:41:52 +0200

    say

---
+++ say/src/lib.rs
@@ -0,0 +1,43 @@
+pub fn encode(n: u64) -> String {
+    let formatter =
+        |num: u64, text: &str| format!("{} {} {}", encode(n / num), text, encode(n % num));
+    match n {
+        0 => "zero".to_string(),
+        1 => "one".to_string(),
+        2 => "two".to_string(),
+        3 => "three".to_string(),
+        4 => "four".to_string(),
+        5 => "five".to_string(),
+        6 => "six".to_string(),
+        7 => "seven".to_string(),
+        8 => "eight".to_string(),
+        9 => "nine".to_string(),
+        10 => "ten".to_string(),
+        11 => "eleven".to_string(),
+        12 => "twelve".to_string(),
+        13 => "thirteen".to_string(),
+        14 => "fourteen".to_string(),
+        15 => "fifteen".to_string(),
+        16 => "sixteen".to_string(),
+        17 => "seventeen".to_string(),
+        18 => "eighteen".to_string(),
+        19 => "nineteen".to_string(),
+        20 => "twenty".to_string(),
+        30 => "thirty".to_string(),
+        40 => "forty".to_string(),
+        50 => "fifty".to_string(),
+        60 => "sixty".to_string(),
+        70 => "seventy".to_string(),
+        80 => "eighty".to_string(),
+        90 => "ninety".to_string(),
+        1..=99 => format!("{}-{}", encode(10 * (n / 10)), encode(n % 10)),
+        1..=999 => formatter(100, "hundred"),
+        1..=999_999 => formatter(1_000, "thousand"),
+        1..=999_999_999 => formatter(1_000_000, "million"),
+        1..=999_999_999_999 => formatter(1_000_000_000, "billion"),
+        1..=999_999_999_999_999 => formatter(1_000_000_000_000, "trillion"),
+        1..=999_999_999_999_999_999 => formatter(1_000_000_000_000_000, "quadrillion"),
+        1..=u64::MAX => formatter(1_000_000_000_000_000_000, "quintillion"),
+    }
+    .replace(" zero", "")
+}
