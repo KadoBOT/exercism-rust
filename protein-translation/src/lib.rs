435e146b524ba0e1cfcba32064ea75a3dbfc7449
KadoBOT <ricardo.ambrogi@gmail.com>
2020-07-12 11:05:29 +0200

    improved protein-translation

--- protein-translation/src/lib.rs
+++ protein-translation/src/lib.rs
@@ -7,38 +7,21 @@ pub struct CodonsInfo<'a> {
 
 impl<'a> CodonsInfo<'a> {
     pub fn name_for(&self, codon: &'a str) -> Option<&'a str> {
-        if let Some(&s) = self.pairs.get(codon) {
-            return Some(s);
-        }
-        None
+        self.pairs.get(codon).cloned()
     }
 
     pub fn of_rna(&self, rna: &'a str) -> Option<Vec<&'a str>> {
-        let subs = rna
-            .as_bytes()
+        rna.as_bytes()
             .chunks(3)
             .map(str::from_utf8)
-            .collect::<Result<Vec<&str>, _>>()
-            .unwrap();
-
-        let translations = subs
-            .iter()
-            .take_while(|c| self.name_for(c) != Some(&"stop codon"))
-            .map(|c| self.name_for(c))
-            .collect::<Vec<_>>();
-
-        if translations.contains(&None) {
-            return None;
-        }
-
-        Some(translations.iter().map(|&c| c.unwrap()).collect())
+            .map(|c| self.name_for(c.unwrap()))
+            .take_while(|&c| c != Some("stop codon"))
+            .collect()
     }
 }
 
 pub fn parse<'a>(pairs: Vec<(&'a str, &'a str)>) -> CodonsInfo<'a> {
-    let pairs = pairs
-        .iter()
-        .map(|(a, b)| (*a, *b))
-        .collect::<HashMap<_, _>>();
-    CodonsInfo { pairs }
+    CodonsInfo {
+        pairs: pairs.into_iter().collect(),
+    }
 }
