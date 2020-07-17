91cba405032e0153f99f10a01f92336b01240194
KadoBOT <ricardo.ambrogi@gmail.com>
2020-07-17 08:42:34 +0200

    sublist improve

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -14,24 +14,15 @@ fn b_contains_a<T: PartialEq>(list_a: &[T], list_b: &[T]) -> bool {
     }
 
     if list_a.len() < list_b.len() {
-        for chunk in list_a.windows(list_a.len()) {
-            for s_chunk in list_b.windows(list_a.len()) {
-                if chunk == s_chunk {
-                    return true;
-                }
-            }
-        }
+        return list_b.windows(list_a.len()).any(|b| b.eq(list_a));
     }
+
     false
 }
 
 pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
     if first_list.eq(second_list) {
         return Comparison::Equal;
-    }
-
-    if first_list.len() == second_list.len() {
-        return Comparison::Unequal;
     }
 
     if b_contains_a(first_list, second_list) {
