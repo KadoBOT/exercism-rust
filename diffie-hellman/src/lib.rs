26f8c4e1a7a4e2b0d94581e8de5f6e3e4da47a7c
KadoBOT <ricardo.ambrogi@gmail.com>
2020-07-27 20:09:05 +0200

    diffie-hellman

---
+++ diffie-hellman/src/lib.rs
@@ -0,0 +1,29 @@
+use rand::prelude::*;
+
+pub fn private_key(p: u64) -> u64 {
+    thread_rng().gen_range(1, p + 1)
+}
+
+pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
+    mod_pow(g, a, p)
+}
+
+pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
+    mod_pow(b_pub, a, p)
+}
+
+fn mod_pow(mut base: u64, mut exp: u64, modulus: u64) -> u64 {
+    if modulus == 1 {
+        return 0;
+    }
+    let mut result = 1;
+    base %= modulus;
+    while exp > 0 {
+        if exp % 2 == 1 {
+            result = result * base % modulus;
+        }
+        exp >>= 1;
+        base = base * base % modulus
+    }
+    result
+}
